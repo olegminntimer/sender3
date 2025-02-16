10cc547a5bb0fc5ef0a6a288d81b000066c0932f
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-02-16 17:11:16 +0500

    add app send

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -11,4 +11,5 @@ class Command(BaseCommand):
         user.is_staff = True
         user.is_superuser = True
         user.save()
+        self.stdout.write(self.style.SUCCESS(f'Successfully created admin user with email {user.email}'))
 
